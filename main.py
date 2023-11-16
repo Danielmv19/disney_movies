44a7bbb777586734413ce6413143caf265bb64b8
Valentine <78097863+Danielmv19@users.noreply.github.com>
2023-11-15 22:26:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,42 +7,7 @@ import pprint
 app = FastAPI()
 app.title = 'Curso FastAPI'
 app.version = '0.2'
-'''
-movies = [
-    {
-    "id": 1,
-    "title": "Avatar",
-    "overview": ":V",
-    "year": "2009",
-    "rating": 7.8,
-    "category": "Accion"
-    },
-    {
-    "id": 2,
-    "title": "Vengadores",
-    "overview": ":V",
-    "year": "2010",
-    "rating": 6,
-    "category": "Aventura"
-    }
-]
-@app.get('/', tags=['home'])
-def message():
-    return HTMLResponse('<h1>hello world</h1>')
 
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
-
-
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int):
-    for item in movies:
-        if item['id'] == id:
-            return item
-    return []
-
-'''
 
 @app.get('/movies/{show_id}', tags=['movies'])
 def get_movies_by_id(show_id:str):
@@ -56,6 +21,6 @@ def get_movies_by_id(show_id:str):
         if show_id in item.get("show_id"):
             item_dict = item
             return item
-            #pprint.pprint(item)
-            #break
-    return []
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
